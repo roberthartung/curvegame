f4fba0a9978dfb21b95b852d054894f3fc1d7f2e
Robert Hartung <r.hartung@roberthartung.de>
2015-02-25 12:15:37 +0100

    Further improvment in the example.

--- example/contains_point.dart
+++ example/contains_point.dart
@@ -1,36 +1,54 @@
-import 'dart:math' as math;
+import 'dart:math';
 import 'dart:html';
-import 'package:curvegame/common.dart' as common;
+import 'package:curvegame/common.dart';
 
-void main() {
-  CanvasElement canvas = querySelector('#canvas');
-  CanvasRenderingContext2D ctx = canvas.getContext('2d');
+CanvasRenderingContext2D ctx;
+CanvasElement canvas;
+ArcSegment arc;
+LineSegment line;
+Point point = new Point(0,0);
+
+void draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
   
-  common.Vector direction = new common.Vector(2,1);
-  math.Point begin = new math.Point(100,100);
-  common.ArcSegment arc = new common.ArcSegment(direction, begin, 4, common.ArcDirection.RIGHT, 25, 90);
-  math.Point begin2 = new math.Point(200,200);
-  common.Vector direction2 = new common.Vector(1.5,1);
-  common.LineSegment line = new common.LineSegment(direction2, begin2, 4, distance: 50);
+  if(arc.containsPoint(point)) {
+    ctx.strokeStyle = 'green';
+  } else {
+    ctx.strokeStyle = 'red';
+  }
   
-  ctx.lineCap = 'square';
-  ctx.strokeStyle = 'red';
-  ctx.lineWidth = arc.width;
   ctx.beginPath();
   arc.draw(ctx);
   ctx.stroke();
   
+  if(line.containsPoint(point)) {
+    ctx.strokeStyle = 'green';
+  } else {
+    ctx.strokeStyle = 'red';
+  }
+  
   ctx.beginPath();
   line.draw(ctx);
   ctx.stroke();
+}
+
+void main() {
+  canvas = querySelector('#canvas');
+  ctx = canvas.getContext('2d');
+  
+  Vector direction = new Vector(2,1);
+  Point begin = new Point(100,100);
+  arc = new ArcSegment(direction, begin, 4, ArcDirection.RIGHT, 25, 90);
+  Point begin2 = new Point(200,200);
+  Vector direction2 = new Vector(1.5,1);
+  line = new LineSegment(direction2, begin2, 4, distance: 50);
+  
+  ctx.lineCap = 'square';
+  ctx.lineWidth = arc.width;
+  draw();
   
   canvas.onMouseMove.listen((MouseEvent ev) {
-    if(arc.containsPoint(ev.offset)) {
-      print(ev.offset);
-    }
-    
-    if(line.containsPoint(ev.offset)) {
-      print(ev.offset);
-    }
+    point = ev.offset;
+    draw();
   });
 }
